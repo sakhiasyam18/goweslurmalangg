2482f6db7d3c7d4d0b781fc0795826bdc0602b80
arrayyandaffa <arrayyandaffa30@gmail.com>
2025-11-07 22:31:29 +0700

    fix: gabung routes saya dan routes asyam

--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,10 @@ use App\Http\Controllers\AdminLoginController; // <-- BARU
 use App\Http\Controllers\DashboardController;  // <-- BARU
 // (Controller AdminController lama dihapus)
 
+use App\Http\Controllers\SepedaController;
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\konfirm;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -53,3 +57,20 @@ Route::middleware('auth')->prefix('admin')->name('admin.')->group(function () {
     // Proses Hitung Denda
     Route::post('/pemesanan/{id}/denda', [DendaController::class, 'store'])->name('denda.store');
 });
+
+// Redirect ke halaman sepeda
+Route::get('/', function () {
+    return redirect()->route('sepeda.index');
+});
+// Semua route CRUD otomatis
+Route::resource('sepeda', SepedaController::class);
+
+Route::get('/konfirm', [konfirm::class, 'index'])->name('konfirm.page');
+// Halaman login
+Route::get('/admin/login', [AdminController::class, 'showLoginForm'])->name('admin.login');
+
+// Proses login
+Route::post('/admin/login', [AdminController::class, 'login'])->name('admin.login.post');
+
+// Dashboard admin (hanya bisa diakses setelah login)
+Route::get('/admin/dashboard', [AdminController::class, 'dashboard'])->name('admin.dashboard')->middleware('auth');
